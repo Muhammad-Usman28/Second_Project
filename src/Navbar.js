17547a4c0c9e42ad9a605008e7599796b2d6e8ca
Muhammad-Usman28 <usmanirfan460@gmail.com>
2024-08-21 23:18:28 +0500

    Responsive for Xl

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,15 +5,24 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 export default function Navbar() {
   return (
     <div>
-      <div className='w-full h-[100px] top-[50px]'>
-      <div className='w-[1120px] h-[45px] left-[160px] relative top-[27px]'>
+      <div className='
+      w-full h-[100px] top-[50px]
+      xl:w-full xl:h-[100px] xl:top-[50px]'>
+      <div className='
+      w-[1120px] h-[45px] left-[160px] relative top-[27px]
+      xl:w-[1120px] xl:h-[45px] xl:left-[160px] xl:relative xltop-[27px]'>
 
-            <div className='w-[60px] h-[36px] top-[4px] absolute left-[10px] pl-2'>
-                <h1 className='font-poppins font-extrabold text-xl size-[24px] leading-[36px]'>Logo</h1>
+            <div className='
+            w-[60px] h-[36px] top-[4px] absolute left-[10px] pl-2
+            xl:w-[60px] xl:h-[36px] xl:top-[4px] xl:absolute xl:left-[10px] xl:pl-2'>
+                <h1 className='
+                font-poppins font-extrabold text-xl size-[24px] leading-[36px]
+                xl:font-poppins xl:font-extrabold xl:text-xl xl:size-[24px] xl:leading-[36px]'>Logo</h1>
             </div>
 
-            <div className='w-[800px] h-[45px]  absolute left-[250px] gap-[28px] flex items-center
-            font-poppins'>
+            <div className='
+            w-[800px] h-[45px]  absolute left-[250px] gap-[28px] flex items-center font-poppins
+            xl:w-[800px] xl:h-[45px] xl:absolute xl:left-[250px] xl:gap-[28px] xl:flex xl:items-center xl:font-poppins'>
             <select name="services" id="services" >
             <option value="" >Service</option>
             </select>
@@ -34,11 +43,19 @@ export default function Navbar() {
             <option value="" >Careers</option>
             </select>
 
-            <span className="border-l-2 border-gray-500 h-8"></span>
+            <span className="
+            border-l-2 border-gray-500 h-8
+            xl:border-l-2 xl:border-gray-500 xl:h-8"></span>
 
-            <div className='w-[250px] h-[45px]  p-[10px, 16px, 10px, 16px]  gap-2 bg-[#E9FBFF] rounded-full'>
-            <FontAwesomeIcon icon={faMagnifyingGlass} className="w-[50px] h-[25px] top-2 absolute" />
-             <h5 className='pt-[10px] pl-[50px] font-roboto size-[14px] text-[#006FFF]'>Search</h5>
+            <div className='
+            w-[250px] h-[45px]  p-[10px, 16px, 10px, 16px]  gap-2 bg-[#E9FBFF] rounded-full
+            xl:w-[250px] xl:h-[45px] xl:p-[10px, 16px, 10px, 16px] xl:gap-2 xl:bg-[#E9FBFF] xl:rounded-fulls'>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="
+            w-[50px] h-[25px] top-2 absolute
+           xl:w-[50px] xl:h-[25px] xl:top-2 xl:absolute" />
+             <h5 className='
+             pt-[10px] pl-[50px] font-roboto size-[14px] text-[#006FFF]
+             xl:pt-[10px] xl:pl-[50px] xl:font-roboto xl:size-[14px] xl:text-[#006FFF]'>Search</h5>
             </div>
 
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,34 +5,53 @@ import { faFacebook, faInstagram, faPinterest, faTwitter } from '@fortawesome/fr
 
 export default function Header() {
   return (
-    <div>
-      <header className='w-full h-[50px] bg-[#202430] '>
+    <div className='w-full'>
+      <header className='
+      w-full h-[50px] bg-[#202430]
+      xl:w-full xl:h-[50px] xl:bg-[#202430]s '>
 
       
-<div className="className= w-[600px] h-[22px]  absolute top-[14px] left-[167px] gap-[42px]
-flex">
+<div className="
+w-[600px] h-[22px]  absolute top-[14px] left-[167px] gap-[42px] flex
+xl:w-[600px] xl:h-[22px]  xl:absolute xl:top-[14px] xl:left-[167px] xl:gap-[42px] xl:flex">
 
-<div className='flex items-center w-[322px] h-[21px]  font-poppins font-[400px] size-[14px] 
-text-white'>
+<div className='
+flex items-center w-[322px] h-[21px]  font-poppins font-[400px] size-[14px] text-white
+xl:flex xl:items-center xl:w-[322px] xl:h-[21px] xl:font-poppins xl:font-[400px] xl:size-[14px] xl:text-white'>
 <FontAwesomeIcon icon={faLocationDot} 
- className="w-[50px] h-[25px] text-[#006FFF]" />
+ className="
+ w-[50px] h-[25px] text-[#006FFF]
+ xl:w-[50px] xl:h-[25px] xl:text-[#006FFF]" />
  24 Olive Street, Prairie, NY 53590
 </div>
 
-<div className='flex items-center w-[250px] h-[21px] font-poppins font-[400px] size-[14px] 
-text-white'>
+<div className='
+flex items-center w-[250px] h-[21px] font-poppins font-[400px] size-[14px] text-white
+xl:flex xl:items-center xl:w-[250px] xl:h-[21px] xl:font-poppins xl:font-[400px] xl:size-[14px]xl:text-white'>
 <FontAwesomeIcon icon={faEnvelope} 
- className="w-[50px] h-[25px] text-[#006FFF]" />
+ className="
+ w-[50px] h-[25px] text-[#006FFF]
+ xl:w-[50px] xl:h-[25px] xl:text-[#006FFF]" />
  whitecollar@gmail.com
 </div>
 
 </div>
 
-<div className='w-[200px] h-[22px] absolute top-[14px] ml-[2000px] '>
-   <FontAwesomeIcon icon={faInstagram} className="w-[50px] h-[25px] text-[#FFFFFF]"/>
-   <FontAwesomeIcon icon={faFacebook} className="w-[50px] h-[25px] text-[#FFFFFF]"/>
-   <FontAwesomeIcon icon={faTwitter} className="w-[50px] h-[25px] text-[#FFFFFF]"/>
-   <FontAwesomeIcon icon={faPinterest} className="w-[50px] h-[25px] text-[#FFFFFF]"/>
+<div className='
+w-[200px] h-[22px] absolute top-[14px] ml-[1000px]
+xl:w-[200px] xl:h-[22px] xl:absolute xl:top-[14px] xl:ml-[1000px] '>
+   <FontAwesomeIcon icon={faInstagram} className="
+   w-[50px] h-[25px] text-[#FFFFFF]
+   xl:w-[50px] xl:h-[25px] xl:text-[#FFFFFF]"/>
+   <FontAwesomeIcon icon={faFacebook} className="
+   w-[50px] h-[25px] text-[#FFFFFF]
+   xl:w-[50px] xl:h-[25px] xl:text-[#FFFFFF]"/>
+   <FontAwesomeIcon icon={faTwitter} className="
+   w-[50px] h-[25px] text-[#FFFFFF]
+   xl:w-[50px] xl:h-[25px] xl:text-[#FFFFFF]"/>
+   <FontAwesomeIcon icon={faPinterest} className="
+   w-[50px] h-[25px] text-[#FFFFFF]
+   xl:w-[50px] xl:h-[25px] xl:text-[#FFFFFF]"/>
 </div>
 </header>
     </div>
